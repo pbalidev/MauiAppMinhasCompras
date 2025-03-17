487eb4d0f7fc7155bd7a470a1b0cb6af08ac952d
PabloRoot <PabloRoot@DESKTOP-72NH8T0>
2025-03-17 06:07:35 -0300

    Criação e implementação de funcionalidades "salvar" dentro do app.

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,6 @@
+using MauiAppMinhasCompras.Models;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +9,25 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+	 
+
+    private async void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_qtde.Text),
+                Preco = Convert.ToDouble(txt_preco.Text),
+            };
+
+            await App.Db.Insert(p);
+            await DisplayAlert("Sucesso", "Produto inserido com sucesso", "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("EPA!", ex.Message, "OK");
+        }
+    }
 }
